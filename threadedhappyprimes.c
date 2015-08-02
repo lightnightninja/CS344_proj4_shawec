84017c190b37550140b4b9f4e3014aa846332622
lightnightninja <mywho09@gmail.com>
2015-08-01 19:26:25 -0700

    I lost all of my code trying to create the last repository -.- Everything.

---
+++ threadedhappyprimes.c
@@ -0,0 +1,9 @@
+//
+//  threadedhappyprimes.c
+//  CS344_proj4_shawec
+//
+//  Created by Cierra Shawe on 8/1/15.
+//  Copyright (c) 2015 Cierra Shawe. All rights reserved.
+//
+
+#include <stdio.h>
